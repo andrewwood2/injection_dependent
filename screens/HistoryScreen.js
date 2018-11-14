49e56f00ce72df91e1ae19b5698d047db55b26af
Harry <harryjp1995@gmail.com>
2018-11-14 15:33:08 +0000

    task: Fixes cancel button test; small refactoring

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Button, Text, TextInput } from 'react-native';
-import axios from 'axios'
-import HistoryTable from '../components/HistoryTable';
+import axios from 'axios';
 import moment from 'moment'
 
 import { connect } from 'react-redux'
+import HistoryTable from '../components/HistoryTable';
 import { saveInj, resetHistory, updateSyncStatus } from '../redux/actions/history';
 
 export class HistoryScreen extends React.Component {
+  static navigationOptions = {
+    title: 'History',
+  };
+
   constructor(props) {
     super(props);
     this.state = {
-      user_id: 'Enter username here...'
-    }
+      user_id: 'Enter username here...',
+    };
   }
-
-  static navigationOptions = {
-    title: 'History'
-  };
 
   prepareLoad() {
     if (this.state.user_id != 'Enter username here...' && this.state.user_id != 'Change me down here') {
-      this.saveData()
-      this.props.resetHistory()
-      this.loadData()
+      this.saveData();
+      this.props.resetHistory();
+      this.loadData();
     } else {
-      this.setState({ user_id: 'Change me down here' })
+      this.setState({ user_id: 'Change me down here' });
     }
   }
 
@@ -33,40 +33,40 @@ export class HistoryScreen extends React.Component {
     if (this.state.user_id != 'Enter username here...' && this.state.user_id != 'Change me down here') {
       this.props.history.forEach((inj) => {
         if (inj.dbsync === false) {
-          axios.post(`https://guarded-caverns-16437.herokuapp.com/injections`, {
+          axios.post('https://guarded-caverns-16437.herokuapp.com/injections', {
             injection: {
               user_id: this.state.user_id,
               site: JSON.stringify(inj.site),
               time: inj.time.unix(),
-              medtype: inj.medType
-            }
-          })
+              medtype: inj.medType,
+            },
+          });
         }
-      })
+      });
       this.props.updateSyncStatus();
     } else {
-      this.setState({ user_id: 'Change me down here' })
+      this.setState({ user_id: 'Change me down here' });
     }
   }
 
   loadData() {
-    self = this
+    self = this;
     axios.get(`https://guarded-caverns-16437.herokuapp.com/injections?user_id=${this.state.user_id}`)
-    .then(data => {
-      for (i in data) {
-        data[i].forEach((inj) => {
-          self.props.saveInj({
-            site: JSON.parse(inj.site),
-            time: moment.unix(parseInt(inj.time, 10)),
-            dbsync: true,
-            medType: inj.medtype
-          })
-        })
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    })
+      .then((data) => {
+        for (i in data) {
+          data[i].forEach((inj) => {
+            self.props.saveInj({
+              site: JSON.parse(inj.site),
+              time: moment.unix(parseInt(inj.time, 10)),
+              dbsync: true,
+              medType: inj.medtype,
+            });
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
@@ -74,16 +74,16 @@ export class HistoryScreen extends React.Component {
       <ScrollView style={styles.container}>
 
         <HistoryTable history={this.props.history} />
-          <Button
-            title={'Load'}
-            id={'load'}
-            onPress={ () => {
-              this.prepareLoad()
+        <Button
+            title="Load"
+            id="load"
+            onPress={() => {
+              this.prepareLoad();
             }}
           />
-          <Button
-            title={'Save'}
-            id={'save'}
+        <Button
+            title="Save"
+            id="save"
             onPress={() => this.saveData()}
           />
         <Text>Username:</Text>
@@ -91,7 +91,7 @@ export class HistoryScreen extends React.Component {
           name="username"
           id="username"
           placeholder={this.state.user_id}
-          onChangeText={ (user_id) => this.setState({ user_id }) }
+          onChangeText={user_id => this.setState({ user_id })}
         />
       </ScrollView>
     );
@@ -102,23 +102,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff'
-  }
+    backgroundColor: '#fff',
+  },
 });
 
 
-const mapStateToProps = (state, ownProps) => {
-  return {
+const mapStateToProps = (state, ownProps) => ({
     history: state.history
-  };
-}
+  });
 
-const mapDispatchToProps = (dispatch) => {
-  return {
+const mapDispatchToProps = (dispatch) => ({
     saveInj: (inj) => { dispatch(saveInj(inj)); },
     updateSyncStatus: () => { dispatch(updateSyncStatus()); },
     resetHistory: () => { dispatch(resetHistory()); }
-  };
-}
+  });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HistoryScreen);
