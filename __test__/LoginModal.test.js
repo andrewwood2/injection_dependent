2b1190590c0c8b34864a098a9ca652ed6733b0cd
Harry <harryjp1995@gmail.com>
2018-11-14 16:03:43 +0000

    test: Tests for fields to be rendered when Log In is pressed

--- __test__/LoginModal.test.js
+++ __test__/LoginModal.test.js
@@ -12,4 +12,22 @@ describe('LoginModal', () => {
   it('Renders a button to login', () => {
     expect(wrapper.find('#login').length).toEqual(1);
   });
+
+  describe('When Log In pressed & modalVisible', () => {
+    beforeEach(() => {
+      wrapper.find('#login').simulate('press');
+    });
+
+    it('Renders a username field', () => {
+      expect(wrapper.find('#username').length).toEqual(1);
+    });
+
+    it('Renders a password field', () => {
+      expect(wrapper.find('#password').length).toEqual(1);
+    });
+
+    it('Renders a button to cancel', () => {
+      expect(wrapper.find('#cancel').length).toEqual(1);
+    });
+  });
 });
